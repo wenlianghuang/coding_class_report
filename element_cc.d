5b301c4b197bfd2d242419959dec2a0dc2d01940
wenliang <wen-liang.huang@cern.ch>
2015-12-25 20:52:01 +0800

    2015.12.25.2051 release

--- element_cc.d
+++
@@ -1,6 +0,0 @@
-
-# DO NOT DELETE
-
-./element_cc.so: element.h
-./element_cc.so: /home/wenliang/root/include/cintdictversion.h /home/wenliang/root/include/RVersion.h
-element_cc__ROOTBUILDVERSION= 5.34/10
